155593b47888d7d752632324c1773f3b05daf515
James Crook <48724197+JoniBach@users.noreply.github.com>
2022-05-30 01:18:18 +0100

    adding path

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import Theme from "./Theme";
 import Button from "./components/button/Button";
 import Surface from "./components/surface/Surface";
 import Tree from "./components/tree/Tree";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <Theme>
       {/* <Surface> */}
       {/* <Button onClick={() => alert("hi")}>this is a button</Button> */}
-      <Tree />
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Tree />}></Route>
+        </Routes>
+      </Router>
       {/* </Surface> */}
     </Theme>
   );
--- src/components/tree/Tree.tsx
+++ src/components/tree/Tree.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 import { SteppedLineTo, Line } from "react-lineto";
 import Button from "../button/Button";
+import { Link } from "react-router-dom";
 
 type TreeProps = {
   children?: any;
@@ -33,35 +34,45 @@ const Group = styled(motion.div)`
 
 const data = [
   {
+    someKey: "test_1",
     someLabel: "test 1",
     someData: [
       {
+        someKey: "test_2",
         someLabel: "test 2",
         someData: [
           {
+            someKey: "test_3",
             someLabel: "test 3",
             someData: [
               {
+                someKey: "test_4",
                 someLabel: "test 4",
                 someData: [
                   {
+                    someKey: "test_5",
                     someLabel: "test 5",
                   },
                   {
+                    someKey: "test_16",
                     someLabel: "test 16",
                   },
                 ],
               },
               {
+                someKey: "test_17",
                 someLabel: "test 17",
               },
               {
+                someKey: "test_20",
                 someLabel: "test 20",
                 someData: [
                   {
+                    someKey: "test_19",
                     someLabel: "test 19",
                   },
                   {
+                    someKey: "test_22",
                     someLabel: "test 22",
                   },
                 ],
@@ -69,34 +80,43 @@ const data = [
             ],
           },
           {
+            someKey: "test_6",
             someLabel: "test 6",
           },
         ],
       },
       {
+        someKey: "test_9",
         someLabel: "test 9",
         someData: [
           {
+            someKey: "test_10",
             someLabel: "test 10",
           },
           {
+            someKey: "test_13",
             someLabel: "test 13",
             someData: [
               {
+                someKey: "test_14",
                 someLabel: "test 14",
                 someData: [
                   {
+                    someKey: "test_15",
                     someLabel: "test 15",
                   },
                   {
+                    someKey: "test_23",
                     someLabel: "test 23",
                   },
                   {
+                    someKey: "test_24",
                     someLabel: "test 24",
                   },
                 ],
               },
               {
+                someKey: "test_21",
                 someLabel: "test 21",
               },
             ],
@@ -110,17 +130,21 @@ const data = [
 type ItemTypes = {
   label: any;
   parentLabel?: any;
+  path?: any;
 };
 type GroupTypes = {
   data: any;
+  path?: any;
   parentLabel?: any;
 };
 
-const TreeItem = ({ label, parentLabel }: ItemTypes) => {
+const TreeItem = ({ label, parentLabel, path }: ItemTypes) => {
   return (
     <>
       <Item className={`${label}`}>
-        <Button>{label}</Button>
+        <Link to={`/${path}`}>
+          <Button>{label}</Button>
+        </Link>
       </Item>
       {parentLabel && (
         <SteppedLineTo
@@ -135,19 +159,22 @@ const TreeItem = ({ label, parentLabel }: ItemTypes) => {
   );
 };
 
-const TreeGroup = ({ data, parentLabel }: GroupTypes) => {
+const TreeGroup = ({ data, parentLabel, path }: GroupTypes) => {
   const hasData = data && data.data && data.data.length > 0;
-  console.log(data);
   return (
     <>
       <Container>
         {data.label && (
-          <TreeItem label={data.label} parentLabel={parentLabel} />
+          <TreeItem label={data.label} parentLabel={parentLabel} path={path} />
         )}
         <Group>
           {hasData ? (
             data.data.map((d: any, i: number) => (
-              <TreeGroup parentLabel={data.label} data={d} />
+              <TreeGroup
+                parentLabel={data.label}
+                data={d}
+                path={`${path ? `${path}/` : ""}${data.key}`}
+              />
             ))
           ) : (
             <>
@@ -162,14 +189,15 @@ const TreeGroup = ({ data, parentLabel }: GroupTypes) => {
 
 const mapForData = "someData";
 const mapForLabel = "someLabel";
+const mapForKey = "someKey";
 
 const Tree = (props: any) => {
   const [mappedData, setMappedData] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   const handleDataMap = (newData: any) => {
     const mapData = newData.map((d: any) => ({
       label: d[mapForLabel],
+      key: d[mapForKey],
       data:
         d[mapForData] && d[mapForData].length > 0
           ? handleDataMap(d[mapForData])
